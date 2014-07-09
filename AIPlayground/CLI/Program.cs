4374bbe56e37503b5c3504d64e93109bae084e62
Franz Haupt <fhaupt@edu.uni-klu.ac.at>
2014-07-09 16:42:46 +0200

    Test operationmonitor

--- AIPlayground/CLI/Program.cs
+++ AIPlayground/CLI/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using AIPlayground.Search.Algorithm;
+using Common;
 
 namespace CLI
 {
@@ -12,6 +15,20 @@ namespace CLI
         static void Main(string[] args)
         {
             //TODO: Insert Problem here!
-        } 
+
+            using (new OperationMonitor("sender", "sleep",time)) 
+            {
+               
+             
+
+            }
+            Console.In.Read();
+
+        }
+
+        private static void time(string result)
+        {
+            Console.Out.WriteLine(result);
+        }
     }
 }
