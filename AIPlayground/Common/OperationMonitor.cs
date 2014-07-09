1e057b2021d7847ad7ab470bff1bbe64d7a3d6f7
Franz Haupt <fhaupt@edu.uni-klu.ac.at>
2014-07-09 16:43:14 +0200

    added library for common things

---
+++ AIPlayground/Common/OperationMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Common
+{
+    public class OperationMonitor : IDisposable
+    {
+        private Object sender;
+        private String operationName;
+        private System.Diagnostics.Stopwatch stopwatch;
+        private Action<string> callback;
+        public OperationMonitor(Object sender, String operationName, Action<string> callback, params object[] subjects)
+        {
+            this.callback = callback;
+            this.sender = sender;
+            this.operationName = operationName;
+            stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            // (sender, Severity.Monitor, "{0} : Start", OperationName + "[" + string.Join(",", subjects) + "]");
+        }
+
+        public void Dispose()
+        {
+            stopwatch.Stop();
+            callback.Invoke(string.Format("{0} {1} : {2:00.000}ms" ,sender, operationName ,  stopwatch.Elapsed.TotalMilliseconds));
+            //Log.Write(sender, Severity.Monitor, "{0} : End {1:00.000}ms", OperationName, Stopwatch.Elapsed.TotalMilliseconds);
+        }
+    }
+}
