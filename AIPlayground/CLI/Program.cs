98689ca8b84d047692b19069fbf79a4b9a2e1801
Daniel Baeck <db@sharpsrc.net>
2014-07-15 13:36:10 +0200

    program using cli control

--- AIPlayground/CLI/Program.cs
+++ AIPlayground/CLI/Program.cs
@@ -7,53 +7,26 @@ using System.Threading;
 using System.Threading.Tasks;
 using AIPlayground.Examples;
 using AIPlayground.Search.Algorithm;
-using AIPlayground.Search.Algorithm.BiDirectionalSearch;
+using AIPlayground.Search.Algorithm.GraphSearch;
 using AIPlayground.Search.Problem;
 using AIPlayground.Search.Problem.State;
+using AIPlayground.Output;
 using Common;
 using AIPlayground;
 
 namespace CLI
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
 
-            //TODO: Insert Problem here!
+		static void Main(string[] args)
+		{
+			//TODO: Insert Problem here!
 
-            SearchNode res = null;
-            using (new OperationMonitor("sender", "sleep",time))
-            {
-              
-				Grid problem = new Grid ("../../Examples/2DGrid/ex1.map");
-
-				foreach(var state in problem.Expand (problem.InitialState))
-					Console.WriteLine (state);
-					
-				SearchAlgorithm sa = new BreadthFirstSearch (problem, new GridState(problem.Goal.X, problem.Goal.Y,0));
-				res = sa.Search ();
-//                MagicSquare problem = new MagicSquare(3);
-//                SearchAlgorithm sa = new DepthFirstSearch(problem);
-//                res = sa.Search();
-//                Console.WriteLine("c: " + problem.c);
-
-            }
-            Console.WriteLine(res);
-			if(res!=null) printPath (res);
-            Console.In.Read();
-
-            
-
-        }
-
-        private static void time(string result)
-        {
-            Console.Out.WriteLine(result);
-        }
-
-		private static void printPath(SearchNode n){
-			Console.WriteLine ("Path: {0}", n.getPath());
+			CLIControl ctrl = new CLIControl ();
+			ctrl.evaluateArgs (args);
+			ctrl.runAlgorithm ();
 		}
-    }
+
+	}
 }
